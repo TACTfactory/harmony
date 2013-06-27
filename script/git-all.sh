673375dfb1ee409956f43285be98ee7e7220d3c7
Mickael Gaillard <mickael.gaillard@tactfactory.com>
2013-06-27 18:34:16 +0200

    devel script

---
+++ script/git-all.sh
@@ -0,0 +1,31 @@
+# pull all repot
+
+cd vendor/tact-core
+git checkout dev
+git pull origin dev
+cd ../..
+
+cd vendor/tact-rest
+git checkout dev
+git pull origin dev
+cd ../..
+
+cd vendor/tact-search
+git checkout dev
+git pull origin dev
+cd ../..
+
+cd vendor/tact-social
+git checkout dev
+git pull origin dev
+cd ../..
+
+cd vendor/tact-symfony
+git checkout dev
+git pull origin dev
+cd ../..
+
+cd vendor/tact-sync
+git checkout dev
+git pull origin dev
+cd ../..
